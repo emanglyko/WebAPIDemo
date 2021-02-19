0028b0d090045ed329a828c9d13e45dc2b07fd8d
Manny <emanglyko@gmail.com>
2021-02-19 13:41:38 +0900

    Added methods to sharedservice to consume our APIs

--- UI/NeptuneUI/src/app/shared.service.ts
+++ UI/NeptuneUI/src/app/shared.service.ts
@@ -6,8 +6,45 @@ import {Observable} from 'rxjs'; //used to hande async requests and responses
   providedIn: 'root'
 })
 export class SharedService {
-readonly APIUrl = "https://localhost:44306/api/";
+readonly APIUrl = "https://localhost:44306/api";
 readonly ImageURL = "https://localhost:44306/Images";
 
   constructor(private http:HttpClient) { }
+
+  GetBeverages():Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/Beverages');
+  }
+
+  AddBeverage(val:any) {
+    return this.http.post(this.APIUrl + '/Beverages', val)
+  }
+
+  UpdateBeverage(val:any) {
+    return this.http.put(this.APIUrl + '/Beverages', val)
+  }
+
+  DeleteBeverage(val:any) {
+    return this.http.delete(this.APIUrl + '/Beverages/' + val)
+  }
+
+  GetBeverageTypes():Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/BeverageTypes');
+  }
+
+  AddBeverageType(val:any) {
+    return this.http.post(this.APIUrl + '/BeverageTypes', val)
+  }
+
+  UpdateBeverageType(val:any) {
+    return this.http.put(this.APIUrl + '/BeverageTypes', val)
+  }
+
+  DeleteBeverageType(val:any) {
+    return this.http.delete(this.APIUrl + '/BeverageTypes/' + val)
+  }
+
+  UploadImage(val:any) {
+    return this.http.post(this.APIUrl + '/Beverages/upload', val)
+  }
+  
 }
